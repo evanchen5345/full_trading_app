78839e1fb255fc00272d60f43c8077c60d451490
Evan Chen <evanchen5345@gmail.com>
2021-05-08 14:21:08 -0700

    removing config from public repo

--- config.py
+++
@@ -1,4 +0,0 @@
-BASE_URL = "https://paper-api.alpaca.markets"
-API_KEY = "PKIM7P2FXWLQPKM1LCDK"
-SECRET_KEY = "hetwk5FhZjlZ8cTM6DtULOA0ftthSFfOxRgL5BkA"
-DB_FILE = "/Users/evanchen/Desktop/Projects/Stocks/Full-Trading-App/app.db"
